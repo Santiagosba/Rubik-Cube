d567cde0e59bc9510c6d907fa496530ac4628778
Santiagosba <119674438+Santiagosba@users.noreply.github.com>
2025-08-31 15:43:31 +0200

    Fix vite base path for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./" // 👈 nombre EXACTO del repo
+  base: "/",
 })
